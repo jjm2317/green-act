896e2c30e8729dc5a29f4302d2a4acb180750392
hyunjin <ally0526@naver.com>
2021-02-04 17:52:54 +0900

    .

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Container from './components/Container';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Login from './components/Login/Login';
 
 console.log(Header);
 const App = () => {
   return (
+    // <Container>
+    //   <Header />
+    //   <Main />
+    //   <Footer />
+    // </Container>
     <Container>
       <Header />
-      <Main />
+      <Login />
       <Footer />
     </Container>
   );
