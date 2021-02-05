7f4c96e4f154c551f6677cc0a7ac2c89fb2c7cc8
hyunjin <ally0526@naver.com>
2021-02-06 00:13:35 +0900

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from './components/Container';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Signup from './components/Signup/Signup';
 import Main from './components/Main/Main';
 import Login from './components/Login/Login';
 
@@ -13,11 +14,23 @@ const App = () => {
     //   <Main />
     //   <Footer />
     // </Container>
+
     <Container>
       <Header />
       <Login />
       <Footer />
+      
+      <Header />
+      <Signup />
+      <Footer />
     </Container>
+
+    // <Container>
+    //   <Header />
+    //   <Signup />
+    //   <Footer />
+    // </Container>
+    
   );
 };
 
