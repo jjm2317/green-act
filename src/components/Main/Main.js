c4052957576dc248172b7c0eead96a68f3e07302
jjm2317 <wlaks2317@gmail.com>
2021-02-13 08:56:09 +0900

    fix: Apply flex to  Main container

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Login from '../Login/Login';
+import SignUp from '../SignUp/SignUp';
 import './Main.scss';
 
 const Main = ({ children }) => {
   return (
     <main className="appMain">
       <Login></Login>
+      {/* <SignUp></SignUp> */}
     </main>
   );
 };
