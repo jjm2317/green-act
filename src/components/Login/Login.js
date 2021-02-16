ffacc970d92e014cc43bdeeb59d350ad04add5d5
hyunjin <ally0526@naver.com>
2021-02-17 00:27:39 +0900

    Merge master brach into hyunjin

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,33 +3,6 @@ import './Login.scss';
 
 const Login = () => {
   return (
-<<<<<<< HEAD
-    <article className="logArticle">
-      <div className="badge">
-          <img src="/images/logLogo.png" alt="green act" />
-      </div>
-      <div className="member">
-      <form action="/my-handling-form-page" method="post">
-        <div className="text-box">
-          <label for="mail">이메일&emsp;&nbsp;▶︎</label>
-          <input type="email" id="mail" name="user_email" />
-        </div>
-        <div className="text-box">
-          <label for="pwd">비밀번호&nbsp;&nbsp;▶︎</label>
-          <input id="pwd" type="password"  name="user_pwd"></input>
-        </div>
-        <button type="submit">Login</button>
-      </form>
-     </div>
-
-      {/* 아이디찾기, 비밀번호 찾기 */}
-
-    </article>
- );
-};
-
-export default Login;
-=======
     <section className="login">
       <h2 className="login-logo">
         <img src="/images/loginLogo.png" alt="green act" />
@@ -78,4 +51,3 @@ export default Login;
 };
 
 export default Login;
->>>>>>> 209465366ae3c9ea2b6606581daa71085ba1b8e8
