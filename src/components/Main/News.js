4159ae39668cf1adcf893ffdbec383b4ffe34257
jjm2317 <wlaks2317@gmail.com>
2021-03-13 20:27:06 +0900

    fix: show news on new page

--- src/components/Main/News.js
+++ src/components/Main/News.js
@@ -1,27 +1,31 @@
 //정적 json파일 브라우저에 렌더링
 //캐러셀 설치 npm install react-slick --save => npm install slick-carousel --save
 
-import React from "react";
-import "./News.scss";
-import mainNews from "./mainNews";
-import Slider from "react-slick";
+import React from 'react';
+import './News.scss';
+import mainNews from './mainNews';
+import Slider from 'react-slick';
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function News() {
   const newsArrayData = mainNews.map((item, index) => {
-      return (
-        <li key={index}>
-          <br/><img src={item.image_url}/>
-          <hr/>
-          <p>
-          {item.title} 
-          <a href={item.url}> 뉴스보기</a>
-          </p>
-        </li>
-      );
-    });
+    return (
+      <li key={index}>
+        <br />
+        <img src={item.image_url} />
+        <hr />
+        <p>
+          {item.title}
+          <a href={item.url} target="_blank">
+            {' '}
+            뉴스보기
+          </a>
+        </p>
+      </li>
+    );
+  });
 
   const settings = {
     dots: true,
@@ -32,32 +36,20 @@ export default function News() {
     arrows: true,
     centerMode: false,
     autoplay: true,
-    autoplaySpeed:2000,
+    autoplaySpeed: 2000,
   };
-  
-    return (
-      <div className="news">
-        {/* <ul className="container">{newsArrayData}</ul> */}
-         <Slider {...settings}>
-          <div>
-            {newsArrayData[7]}
-          </div>
-          <div>
-            {newsArrayData[6]}
-          </div>
-          <div>
-            {newsArrayData[2]}
-          </div>
-          <div>
-            {newsArrayData[3]}
-          </div>
-          <div>
-            {newsArrayData[4]}
-          </div>
-          <div>
-            {newsArrayData[5]}
-          </div>
-         </Slider>
-      </div>
-    );
-  }
+
+  return (
+    <div className="news">
+      {/* <ul className="container">{newsArrayData}</ul> */}
+      <Slider {...settings}>
+        <div>{newsArrayData[7]}</div>
+        <div>{newsArrayData[6]}</div>
+        <div>{newsArrayData[2]}</div>
+        <div>{newsArrayData[3]}</div>
+        <div>{newsArrayData[4]}</div>
+        <div>{newsArrayData[5]}</div>
+      </Slider>
+    </div>
+  );
+}
