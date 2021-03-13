4a08718bed93124cbe122c48c8bb88e06edcd49f
eunbicho <puppy101976@naver.com>
2021-03-13 18:41:59 +0900

    feat: making GeneralWaste page UI (not finished)

---
+++ src/components/Recycle/GeneralWaste.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './GeneralWaste.scss';
+
+const GeneralWaste = () => {
+  return (
+    <section className="GeneralWaste">
+      <div>일반쓰레기</div>
+    </section>
+  );
+};
+
+export default GeneralWaste;
