abb39c87d3ee65e9f30c3fd153b2624419979e2e
eunbicho <puppy101976@naver.com>
2021-03-15 04:09:30 +0900

    Fix: Link menu image and each page in recyclePage

--- src/components/Recycle/RecyclePage.js
+++ src/components/Recycle/RecyclePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './RecyclePage.scss';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import LoginForm from '../../containers/auth/LoginForm';
 import GeneralWaste from './GeneralWaste';
 import GeneralWasteImage from "../../images/recycleMenu_generalWaste.png"
@@ -11,63 +11,68 @@ import PlasticBagsImage from "../../images/recycleMenu_Vinyl.png"
 import StyrofoamTrashImage from "../../images/recycleMenu_Styrofoam.png"
 import PaperWasteImage from "../../images/recycleMenu_Paper.png"
 import FoodWasteImage from "../../images/recycleMenu_Food.png"
+import SignUp from '../SignUp/SignUp';
+import Bestboard from '../Main/Bestboard';
+import PlasticWaste from './PlasticWaste';
 
 const RecyclePage = () => {
   return(
     <section className = "RecyclePage">
       <ul className= "menu-list">
         <li className= "menu-item">
-        <Link to='recycle/GeneralWaste'>
+        <Link to="/recycle/GeneralWaste">
         <img src={GeneralWasteImage} alt="generalWaste"/>
         </Link>
         </li>
 
         <li className= "menu-item">
-        <Link to="recycle/GlassTrash">
+        <Link to="/recycle/GlassTrash">
           <img src={GlassTrashImage} alt="glassTrash"/>
         </Link>
         </li>
 
         <li className= "menu-item">
-        <Link to="recycle/CannedGarbage">
+        <Link to="/recycle/CannedGarbage">
           <img src={CannedGarbageImage} alt="cannedGarbage"/>
         </Link>
         </li>
 
         <li className= "menu-item">
-        <Link to="recycle/PlasticWaste">
+        <Link to="/recycle/PlasticWaste">
           <img src={PlasticWasteImage} alt="plasticWaste"/>
         </Link>
         </li>
 
         <li className= "menu-item">
-          <Link to="recycle/PlasticBags">
+          <Link to="/recycle/PlasticBags">
             <img src={PlasticBagsImage} alt="plasticBags"/>
           </Link>
         </li>
 
         <li className= "menu-item">
-          <Link to="recycle/PaperWaste">
+          <Link to="/recycle/PaperWaste">
             <img src={PaperWasteImage} alt="paperWaste"/>
           </Link>
         </li>
 
         <li className= "menu-item">
-          <Link to="recycle/StyrofoamTrash">
+          <Link to="/recycle/StyrofoamTrash">
             <img src={StyrofoamTrashImage} alt="styrofoamTrash"/>
           </Link>
         </li>
 
         <li className= "menu-item">
-          <Link to="recycle/FoodWaste">
+          <Link to="/recycle/FoodWaste">
             <img src={FoodWasteImage} alt="foodWaste"/>
           </Link>
         </li>
 
       </ul>
-      <div className= "description">
 
+      <div className= "description">
+          <Route exact path="/recycle/GeneralWaste" component={GeneralWaste} />
       </div>
+
     </section>
 
   );
