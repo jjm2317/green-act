24681b740d9b4ea00230005c43c23ece25ec787c
jjm2317 <wlaks2317@gmail.com>
2021-03-20 08:05:49 +0900

    feat: redux container signup

--- src/containers/auth/SignUpForm.js
+++ src/containers/auth/SignUpForm.js
@@ -5,7 +5,38 @@ import SignUp from '../../components/SignUp/SignUp';
 
 const SignUpForm = () => {
   const dispatch = useDispatch();
+  //useSelector: 리덕스의 상태값을 조회하는 함수
   const { form } = useSelector(({ auth }) => ({
     form: auth.signUp,
   }));
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    //스토어에 액션을 전달한다.
+    dispatch(
+      //액션 생성함수: 액션을 반환
+      changeField({
+        form: 'signUp',
+        key: name,
+        value,
+      })
+    );
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+  }
+
+  useEffect(() => {
+    dispatch(initializeForm('signUp'));
+  }, [dispatch]);
+
+  return (
+    <SignUp
+      type="signUp",
+      form={form},
+      onChange={onChange},
+      onSubmit={onSubmit}
+    />
+  )
 };
