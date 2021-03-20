c317984216a56c6dce02b18c0a435746ce6f3681
jjm2317 <wlaks2317@gmail.com>
2021-03-20 13:57:38 +0900

    feat: locate Main when login

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm } from '../../modules/auth';
 import Login from '../../components/Login/Login';
 //로그인관련 데이터를 props로서 데이터 처리능력이 없는 Login component로 전달
-const LoginForm = () => {
+const LoginForm = ({ history }) => {
   console.log('loginform start');
   const dispatch = useDispatch();
   const { form } = useSelector(({ auth }) => ({
@@ -26,13 +26,14 @@ const LoginForm = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:4000/login', {
+    const response = await fetch('http://3.34.157.240:4000/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(form),
     });
     const { username, password } = await response.json();
-    dispatch();
+
+    history.push('/');
   };
 
   useEffect(() => {
