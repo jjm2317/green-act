5498abb7b6a2194aa2352388e8628fe28396d497
jjm2317 <wlaks2317@gmail.com>
2021-03-24 21:36:45 +0900

    feat: Create user redux module

---
+++ src/modules/user.js
@@ -0,0 +1,6 @@
+import { createAction, handleActions } from 'redux-actions';
+import { takeLatest } from 'redux-saga/effects';
+import * as authAPI from '../lib/api/auth';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/createRequestSaga';
