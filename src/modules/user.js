013b4df080efbc5b922fade2118e433ae5bf1ff6
jjm2317 <wlaks2317@gmail.com>
2021-03-25 08:58:08 +0900

    feat: complete redux module for user check

--- src/modules/user.js
+++ src/modules/user.js
@@ -4,3 +4,47 @@ import * as authAPI from '../lib/api/auth';
 import createRequestSaga, {
   createRequestActionTypes,
 } from '../lib/createRequestSaga';
+
+//사용자의 상태를 담는 리덕스 모듈
+
+const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
+
+const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
+  'user/CHECK'
+);
+const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
+  'user/CHECK'
+);
+
+export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
+export const check = createAction(CHECK);
+
+const checkSaga = createRequestSaga(CHECK, authAPI.check);
+export function* userSaga() {
+  yield takeLatest(CHECK, checkSaga);
+}
+
+const initialState = {
+  user: null,
+  checkError: null,
+};
+
+export default handleActions(
+  {
+    [TEMP_SET_USER]: (state, { payload: user }) => ({
+      ...state,
+      user,
+    }),
+    [CHECK_SUCCESS]: (state, { payload: user }) => ({
+      ...state,
+      user,
+      checkError: null,
+    }),
+    [CHECK_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      user: null,
+      checkError: error,
+    }),
+  },
+  initialState
+);
