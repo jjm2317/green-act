2da2d81b37691db6757da3b8b300837add36533a
jjm2317 <wlaks2317@gmail.com>
2021-03-24 21:30:05 +0900

    feat: SignUp

--- src/containers/auth/SignUpForm.js
+++ src/containers/auth/SignUpForm.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, signUp } from '../../modules/auth';
 import SignUp from '../../components/SignUp/SignUp';
 
 const SignUpForm = () => {
   const dispatch = useDispatch();
   //useSelector: 리덕스의 상태값을 조회하는 함수
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError } = useSelector(({ auth }) => ({
     form: auth.signUp,
+    auth: auth.auth,
+    authError: auth.authError,
   }));
 
   const onChange = (e) => {
@@ -25,11 +27,30 @@ const SignUpForm = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const { nickname, username, password, passwordConfirm, interest } = form;
+    if (password !== passwordConfirm) {
+      //에러처리
+      return;
+    }
+
+    dispatch(signUp({ username, password, nickname, interest }));
   };
 
   useEffect(() => {
     dispatch(initializeForm('signUp'));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log('authError');
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log('signupSuccess');
+      console.log(auth);
+    }
+  }, [auth, authError]);
 
   return (
     <SignUp type="signUp" form={form} onChange={onChange} onSubmit={onSubmit} />
