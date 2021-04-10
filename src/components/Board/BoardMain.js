bbe6c06f0e74276399c08e2d298493a9f12c5b9c
eunbicho <puppy101976@naver.com>
2021-04-10 14:02:31 +0900

    feat: 게시판 UI 진행중

--- src/components/Board/BoardMain.js
+++ src/components/Board/BoardMain.js
@@ -3,8 +3,15 @@ import './BoardMain.scss';
 
 const BoardMain = () => {
   return (
-    <div>
-
+    <div className="Board">
+      <section className="top">
+        <form className="board_form">
+          <input className="searchbar" placeholder="원하는 품목의 분리수거 방법을 검색해보세요"></input>
+          <button className="button">검색</button>
+        </form>
+      </section>
+      <section className="bottom">
+      </section>
     </div>
   );
 };
