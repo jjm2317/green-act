ab8bc3a72e9ef14d8edc688c2508d4d27fe632b1
eunbicho <puppy101976@naver.com>
2021-04-10 14:13:08 +0900

    feat: 게시판 UI 진행중

--- src/components/Board/BoardMain.js
+++ src/components/Board/BoardMain.js
@@ -6,7 +6,7 @@ const BoardMain = () => {
     <div className="Board">
       <section className="top">
         <form className="board_form">
-          <input className="searchbar" placeholder="원하는 품목의 분리수거 방법을 검색해보세요"></input>
+          <input className="searchbar" placeholder="# 분리수거 Tip # 서울시 분리수거"></input>
           <button className="button">검색</button>
         </form>
       </section>
