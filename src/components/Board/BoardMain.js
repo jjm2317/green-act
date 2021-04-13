cbd2a7f2e433d1c0604c5c4ba858b4b7b245f3b3
eunbicho <puppy101976@naver.com>
2021-04-14 01:31:22 +0900

    feat: 게시판 UI 진행중

--- src/components/Board/BoardMain.js
+++ src/components/Board/BoardMain.js
@@ -13,14 +13,40 @@ const BoardMain = () => {
       <section className="bottom">
         <section className="left">
           <div className="popularBoard">
-
+            <div className="headline">
+              <div className="title">인기 게시물</div>
+              <button className="more">더보기</button>
+            </div>
           </div>
           <div className="recentBoard">
-
+            <div className="headline">
+              <div className="title">최근 게시물</div>
+              <button className="more">더보기</button>
+            </div>
           </div>
 
         </section>
         <section className="right">
+          <div className="mypage-box">
+            <div className="myarticle">
+              <div className="myarticle_image"></div>
+              내가 쓴 글
+              <div className="article_number">n</div>
+            </div>
+            <div className="myscrap">
+              <div className="myscrap_image"></div>
+              스크랩
+              <div className="scrap_number">n</div>
+            </div>
+            <div className="mycomment">
+              <div className="mycomment_image"></div>
+              댓글 단 글
+              <div className="comment_number">n</div>
+            </div>
+          </div>
+          <div className="category-box">
+
+          </div>
 
         </section>
       </section>
