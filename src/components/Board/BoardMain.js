168c74a8f8f28fcd4b7cb961f7a9ec9b1a160704
eunbicho <puppy101976@naver.com>
2021-04-18 19:55:43 +0900

    feat: Modify board UI

--- src/components/Board/BoardMain.js
+++ src/components/Board/BoardMain.js
@@ -17,14 +17,57 @@ const BoardMain = () => {
               <div className="title">인기 게시물</div>
               <button className="more">더보기</button>
             </div>
+            <div className="popular-itemList">
+              <div className="popular-item">
+                <section className="popular-left">
+                  <div className="pItem-title">분리배출 tip</div>
+                  <div className="pItem-content">게시글 첫 줄...</div>
+                </section>
+                <section className="popular-right">
+                  <div className="pItem-comment">댓글 10</div>
+                  <div className="pItem-scrap">스크랩 10</div>
+                </section>
+              </div>
+              <div className="popular-item item-last">
+                <section className="popular-left">
+                  <div className="pItem-title">분리배출 실천</div>
+                  <div className="pItem-content">게시글 첫 줄...</div>
+                </section>
+                <section className="popular-right">
+                  <div className="pItem-comment">댓글 10</div>
+                  <div className="pItem-scrap">스크랩 10</div>
+                </section>
+              </div>
+            </div>
           </div>
           <div className="recentBoard">
             <div className="headline">
               <div className="title">최근 게시물</div>
               <button className="more">더보기</button>
             </div>
+            <div className="recent-itemList">
+              <div className="recent-item">
+                <section className="recent-left">
+                  <div className="rItem-title">최근 게시물 1</div>
+                  <div className="rItem-content">게시글 첫 줄...</div>
+                </section>
+                <section className="recent-right">
+                  <div className="rItem-comment">댓글 10</div>
+                  <div className="rItem-scrap">스크랩 10</div>
+                </section>
+              </div>
+              <div className="recent-item item-last">
+                <section className="recent-left">
+                  <div className="rItem-title">최근 게시물 2</div>
+                  <div className="rItem-content">게시글 첫 줄...</div>
+                </section>
+                <section className="recent-right">
+                  <div className="rItem-comment">댓글 10</div>
+                  <div className="rItem-scrap">스크랩 10</div>
+                </section>
+              </div>
+            </div>
           </div>
-
         </section>
         <section className="right">
           <div className="mypage-box">
@@ -45,6 +88,15 @@ const BoardMain = () => {
             </div>
           </div>
           <div className="category-box">
+            <div className="headline">
+              <div className="title">카테고리</div>
+            </div>
+            <div className="item-list">
+              <div className="item item-recycle">재활용</div>
+              <div className="item item-campaign">캠페인</div>
+              <div className="item item-news">뉴스</div>
+              <div className="item item-wasteSorting">분리수거</div>
+            </div>
 
           </div>
 
