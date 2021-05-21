805f732b7647375926ef9138d9b4874b2b89926a
eunbicho <puppy101976@naver.com>
2021-05-21 16:11:39 +0900

    feat: (Popularitem) Divide component using styled-components

---
+++ src/components/Board/PopularItemList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PopularItem from './PopularItem';
+import styled from 'styled-components';
+
+const List = styled.div`
+    width: 100%;
+    height: 14.5rem;
+`
+const PopularItemList = () => {
+  return (
+    <List>
+      {/* 인기 게시물 item 목록 */}
+      <PopularItem title="제목" content="내용" comment="5" scrap="10" last="1px #b4df93 solid"/>
+      <PopularItem title="제목2" content="내용2" comment="10" scrap="5" last="none"/>
+    </List>
+  );
+};
+
+export default PopularItemList
